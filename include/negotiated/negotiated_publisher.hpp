0c8663547bb558de4252774453940a9c0380edcb
Chris Lalancette <clalancette@openrobotics.org>
2022-01-12 14:51:20 +0000

    Add NegotiatedPublisher skeleton.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

---
+++ include/negotiated/negotiated_publisher.hpp
@@ -0,0 +1,38 @@
+// Copyright 2022 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NEGOTIATED__NEGOTIATED_PUBLISHER_HPP_
+#define NEGOTIATED__NEGOTIATED_PUBLISHER_HPP_
+
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/empty.hpp"
+#include "std_srvs/srv/empty.hpp"
+
+namespace negotiated
+{
+
+class NegotiatedPublisher
+{
+public:
+  explicit NegotiatedPublisher(rclcpp::Node & node, const std::string & topic_name);
+
+private:
+  rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr publisher_;
+};
+
+}  // namespace negotiated
+
+#endif  // NEGOTIATED__NEGOTIATED_PUBLISHER_HPP_
