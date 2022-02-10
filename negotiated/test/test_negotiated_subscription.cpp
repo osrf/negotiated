cbb8df11cfa80c4bf46e6c44706b5684f1da8870
Chris Lalancette <clalancette@openrobotics.org>
2022-02-10 22:12:16 +0000

    Add in tests for NegotiatedSubscription remove_supported_callback.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- negotiated/test/test_negotiated_subscription.cpp
+++ negotiated/test/test_negotiated_subscription.cpp
@@ -120,6 +120,13 @@ TEST_F(TestNegotiatedSubscription, add_duplicate_callback)
   EXPECT_THROW(sub.add_supported_callback<EmptyT>(1.0, rclcpp::QoS(10), cb), std::runtime_error);
 }
 
+TEST_F(TestNegotiatedSubscription, remove_nonexistent_callback)
+{
+  negotiated::NegotiatedSubscription sub(*node_, "foo");
+
+  EXPECT_THROW(sub.remove_supported_callback<EmptyT>(), std::runtime_error);
+}
+
 TEST_F(TestNegotiatedSubscription, add_single_callback)
 {
   // Setup of our dummy publisher
@@ -612,6 +619,105 @@ TEST_F(TestNegotiatedSubscription, custom_negotiated_cb)
       return string_count > 0;
     };
   ASSERT_TRUE(spin_while_waiting(count_break_func));
+
+  ASSERT_EQ(empty_count, 0);
+  ASSERT_EQ(string_count, 1);
+}
+
+TEST_F(TestNegotiatedSubscription, renegotiate_after_remove_topic)
+{
+  // Setup of our dummy publisher
+  auto data_pub = node_->create_publisher<std_msgs::msg::String>("foo/b", rclcpp::QoS(10));
+
+  auto data_pub2 = node_->create_publisher<std_msgs::msg::Empty>("foo/a", rclcpp::QoS(10));
+
+  negotiated_interfaces::msg::NegotiatedTopicsInfo topics_msg;
+  topics_msg.success = true;
+
+  negotiated_interfaces::msg::NegotiatedTopicInfo topic_info1;
+  topic_info1.ros_type_name = "std_msgs/msg/Empty";
+  topic_info1.supported_type_name = "a";
+  topic_info1.topic_name = "foo/a";
+  topics_msg.negotiated_topics.push_back(topic_info1);
+
+  negotiated_interfaces::msg::NegotiatedTopicInfo topic_info2;
+  topic_info2.ros_type_name = "std_msgs/msg/String";
+  topic_info2.supported_type_name = "b";
+  topic_info2.topic_name = "foo/b";
+  topics_msg.negotiated_topics.push_back(topic_info2);
+
+  // Setup and test the subscription
+  auto sub = std::make_shared<negotiated::NegotiatedSubscription>(*node_, "foo");
+
+  int empty_count = 0;
+  auto empty_cb = [&empty_count](const std_msgs::msg::Empty & msg)
+    {
+      (void)msg;
+      empty_count++;
+    };
+
+  int string_count = 0;
+  auto string_cb = [&string_count](const std_msgs::msg::String & msg)
+    {
+      (void)msg;
+      string_count++;
+    };
+
+  sub->add_supported_callback<EmptyT>(1.0, rclcpp::QoS(10), empty_cb);
+  sub->add_supported_callback<StringT>(0.5, rclcpp::QoS(10), string_cb);
+
+  sub->start();
+
+  auto negotiated_break_func = [this, sub]() -> bool
+    {
+      return sub->get_negotiated_topic_publisher_count() > 0 &&
+             topics_pub_->get_subscription_count() > 0;
+    };
+  ASSERT_TRUE(spin_while_waiting(negotiated_break_func));
+
+  topics_pub_->publish(topics_msg);
+
+  auto data_break_func = [sub, data_pub2]() -> bool
+    {
+      return sub->get_data_topic_publisher_count() > 0 && data_pub2->get_subscription_count() > 0;
+    };
+  ASSERT_TRUE(spin_while_waiting(data_break_func));
+
+  std_msgs::msg::Empty data;
+  data_pub2->publish(data);
+
+  auto count_break_func = [&empty_count]() -> bool
+    {
+      return empty_count > 0;
+    };
+  ASSERT_TRUE(spin_while_waiting(count_break_func));
+
+  ASSERT_EQ(empty_count, 1);
+  ASSERT_EQ(string_count, 0);
+
+  // Now remove the NegotiatedSubscription Empty topic
+  sub->remove_supported_callback<EmptyT>();
+
+  // Now renegotiate; the subscription should change to String
+  empty_count = 0;
+  string_count = 0;
+
+  topics_pub_->publish(topics_msg);
+
+  auto data_break_func2 = [sub, data_pub]() -> bool
+    {
+      return sub->get_data_topic_publisher_count() > 0 && data_pub->get_subscription_count() > 0;
+    };
+  ASSERT_TRUE(spin_while_waiting(data_break_func2));
+
+  std_msgs::msg::String data2;
+  data_pub->publish(data2);
+
+  auto count_break_func2 = [&string_count]() -> bool
+    {
+      return string_count > 0;
+    };
+  ASSERT_TRUE(spin_while_waiting(count_break_func2));
 
   ASSERT_EQ(empty_count, 0);
   ASSERT_EQ(string_count, 1);
