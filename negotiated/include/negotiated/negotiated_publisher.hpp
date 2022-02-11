46e7374e2e3e8a2f79d21e7f25d208c24b9c9824
Chris Lalancette <clalancette@openrobotics.org>
2022-02-11 19:57:28 +0000

    Add doxygen comments.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- negotiated/include/negotiated/negotiated_publisher.hpp
+++ negotiated/include/negotiated/negotiated_publisher.hpp
@@ -35,17 +35,43 @@
 namespace negotiated
 {
 
+/// NegotiatedPublisherOptions allows the user to control some aspects of the NegotiatedPublisher.
 struct NegotiatedPublisherOptions final
 {
+  /// Whether to automatically cause a renegotiation when NegotiatedSubscriptions leave the graph.
+  /// If set to false, then renegotiations will only happen when the user calls
+  // NegotiatedPublisher::negotiate().
   bool negotiate_on_subscription_removal{true};
+
+  /// Whether to automatically cause a renegotiation when NegotiatedSubscriptions join the graph.
+  /// If set to false, then renegotiations will only happen when the user calls
+  // NegotiatedPublisher::negotiate().
   bool negotiate_on_subscription_add{true};
 };
 
+/// NegotiatedPublisher implements the publishing side of a negotiated system.
+/// It is ultimately responsible for determining which publishers and subscriptions in a system
+/// get created.
 class NegotiatedPublisher
 {
 public:
   RCLCPP_SMART_PTR_DEFINITIONS(NegotiatedPublisher)
 
+  /// Create a new NegotiatedPublisher with the given "base" topic_name.
+  /**
+   * The topic_name given here will be used to determine the list of NegotiatedSubscriptions in the
+   * network.  It will also be used to communicate the chosen topic_name at the end of negotiation.
+   *
+   * \param[in] node_parameters The node parameters interface to use.
+   * \param[in] node_topics The node topics interface to use.
+   * \param[in] node_logging The node logging interface to use.
+   * \param[in] node_graph The node graph interface to use.
+   * \param[in] node_base The node base interface to use.
+   * \param[in] node_timers The node timers interface to use.
+   * \param[in] topic_name The topic name to use for the "base" topic and as the prefix for the
+   *                       final topic name.
+   * \param[in] negotiated_pub_options The options to use.
+   */
   explicit NegotiatedPublisher(
     rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters,
     rclcpp::node_interfaces::NodeTopicsInterface::SharedPtr node_topics,
@@ -56,6 +82,16 @@ public:
     const std::string & topic_name,
     const NegotiatedPublisherOptions & negotiated_pub_options = NegotiatedPublisherOptions());
 
+  /// Create a new NegotiatedPublisher with the given "base" topic_name.
+  /**
+   * The topic_name given here will be used to determine the list of NegotiatedSubscriptions in the
+   * network.  It will also be used to communicate the chosen topic_name at the end of negotiation.
+   *
+   * \param[in] node The node to use to create the publishers and subscriptions.
+   * \param[in] topic_name The topic name to use for the "base" topic and as the prefix for the
+   *                       final topic name.
+   * \param[in] negotiated_pub_options The options to use.
+   */
   template<typename NodeT>
   explicit NegotiatedPublisher(
     NodeT & node,
@@ -73,6 +109,29 @@ public:
   {
   }
 
+  /// Add a supported type with a weight.
+  /**
+   * Add a supported type to this NegotiatedPublisher.  The template argument must have the form:
+   *
+   * struct MyType {
+   *   using MsgT = my_ros::msg::type;
+   *   static const inline std::string supported_type_name = "arbitrary";
+   * };
+   *
+   * As a concrete example:
+   *
+   * struct MyType {
+   *   using MsgT = std_msgs::msg::String;
+   *   static const inline std::string supported_type_name = "arbitrary";
+   * };
+   *
+   * \param[in] weight - The relative weight to assign this supported type; types with higher
+   *                     weights are more likely to be chosen.
+   * \param[in] qos - The Quality of Service parameters to apply if this type is chosen.
+   * \param[in] options - The PublisherOptions to apply if this type is chosen.
+   * \throws std::runtime_error if the supported_type_name in the structure is the empty string, OR
+   * \throws std::runtime_error if the exact same type has already been added.
+   */
   template<typename T>
   void add_supported_type(
     double weight,
@@ -109,6 +168,14 @@ public:
     key_to_supported_types_[key_name].supported_type_name = T::supported_type_name;
   }
 
+  /// Remove a supported type.
+  /**
+   * Remove a supported type from this NegotiatedPublisher.  The template argument must have the
+   * same form as in add_supported_type(), and must have previously been added by
+   * add_supported_type().
+   * \throws std::runtime_error if the supported_type_name in the structure is the empty string, OR
+   * \throws std::runtime_error if the type to be removed was not previously added.
+   */
   template<typename T>
   void remove_supported_type()
   {
@@ -125,10 +192,31 @@ public:
     key_to_supported_types_.erase(key_name);
   }
 
+  /// Start collecting information from the attached NegotiatedSubscriptions.
+  /**
+   * Until this method is called, no data from NegotiatedSubscriptions will be collected.
+   * This is to give the user the chance to call add_supported_type() for each of the publisher
+   * types to support.  Once that has been done, the user should call start().
+   */
   void start();
 
+  /// Negotiate between this NegotiatedPublisher and all connected NegotatiedSubscriptions.
+  /**
+   * This method can be called by the user at any time to negotiate or renegotiate.
+   * If the default values for NegotiatedPublisherOptions are taken, this will automatically
+   * get called any time a NegotiatedSubscriptions joins or leaves the network.
+   */
   void negotiate();
 
+  /// Determine whether a particular type was chosen by negotiation.
+  /**
+   * This method allows the user to determine whether a particular type registered
+   * with add_supported_type() has been chosen by negotiation.  Note that negotiation may choose
+   * more than one type.  The template argument here should be one of the same ones
+   * as used in add_supported_type().
+   *
+   * \return Whether the particular type was chosen by negotiation.
+   */
   template<typename T>
   bool type_was_negotiated()
   {
@@ -137,6 +225,18 @@ public:
     return key_to_publisher_.count(key) > 0;
   }
 
+  /// Publish constref data to a chosen publisher.
+  /**
+   * This method allows the user to publish data using constref to a chosen publisher.
+   * This will only succeed if a publisher of that particular type has been chosen by
+   * negotiation; call type_was_negotiated() first to determine this.
+   * If a publisher of that particular type was not chosen, this method returns without doing
+   * any work.
+   * If a publisher of that particular type was chosen, this method publishes the data as
+   * requested.
+   *
+   * \param[in] msg The message to publish.
+   */
   template<typename T>
   void publish(const typename T::MsgT & msg)
   {
@@ -155,6 +255,18 @@ public:
     pub->publish(msg);
   }
 
+  /// Publish std::unique_ptr data to a chosen publisher.
+  /**
+   * This method allows the user to publish data using constref to a chosen publisher.
+   * This will only succeed if a publisher of that particular type has been chosen by
+   * negotiation; call type_was_negotiated() first to determine this.
+   * If a publisher of that particular type was not chosen, this method returns without doing
+   * any work.
+   * If a publisher of that particular type was chosen, this method publishes the data as
+   * requested.
+   *
+   * \param[in] msg The message to publish.
+   */
   template<typename T>
   void publish(std::unique_ptr<typename T::MsgT> msg)
   {
@@ -173,6 +285,11 @@ public:
     pub->publish(std::move(msg));
   }
 
+  /// Stop the flow of data and forget about current negotiation.
+  /**
+   * The opposite of start(), this method can be called to stop the flow of all data
+   * and have this NegotiatedPublisher forget about any negotiation.
+   */
   void stop();
 
 private:
@@ -180,35 +297,76 @@ private:
 
   struct SupportedTypeInfo final
   {
+    /// A map of PublisherGids to the weight associated with them.  Note that this is unique since
+    /// every NegotiatedSubscription in the network has its own publisher for suppported types.
     std::map<PublisherGid, double> gid_to_weight;
+    /// The canonical ROS type name associated with this type.
     std::string ros_type_name;
+    /// The arbitrary supported_type_name string associated with this type.
     std::string supported_type_name;
+    /// The factory function associated with this type.
     std::function<rclcpp::PublisherBase::SharedPtr(const std::string &)> pub_factory;
   };
 
+  /// The timer callback to use to keep an eye on the network graph.
+  /**
+   * This method is run periodically to see if there have been changes in the network,
+   * specifically NegotiatedSubscriptions that have dropped off.  If there are, they will be
+   * removed from the set of participants in the negotiation, and, by default, renegotiation
+   * will happen.
+   */
   void timer_callback();
 
+  /// Generate the key that is used as an index into the maps.
+  /**
+   * Two of the internal maps are keyed off of the uniqueness of individual types as given by
+   * add_supported_type().  This method is responsible for generating those unique keys.
+   *
+   * \param[in] ros_type_name The canonical ROS type name, like std_msgs/msg/String.
+   * \param[in] supported_type_name The arbitrary (but non-empty) string passed by a user.
+   * \return A string that is the unique key into the maps.
+   */
   std::string generate_key(
     const std::string & ros_type_name,
     const std::string & supported_type_name);
 
+  /// The node parameters interface to use.
   rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters_;
+  /// The node topics interface to use.
   rclcpp::node_interfaces::NodeTopicsInterface::SharedPtr node_topics_;
+  /// The node logging interface to use.
   rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logging_;
+  /// The node graph interface to use.
   rclcpp::node_interfaces::NodeGraphInterface::SharedPtr node_graph_;
+  /// The node base interface to use.
   rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base_;
+  /// The node timers interface to use.
   rclcpp::node_interfaces::NodeTimersInterface::SharedPtr node_timers_;
+  /// The original topic_name provided by the user.
   std::string topic_name_;
+  /// The original options to this class provided by the user.
   NegotiatedPublisherOptions negotiated_pub_options_;
 
+  /// A map between unique type keys (as returned by generate_key()) and SupportedTypeInfos.
   std::map<std::string, SupportedTypeInfo> key_to_supported_types_;
+  /// The publisher used to collect NegotiatedSubscriptions that are part of the network and for
+  /// informing those NegotiatedSubscriptions of the chosen types.
   rclcpp::Publisher<negotiated_interfaces::msg::NegotiatedTopicsInfo>::SharedPtr
     negotiated_publisher_;
+  /// A map between unique type keys (as returned by generate_key()) and an associated
+  /// rclcpp::Publisher.  Will only be populated if the particular type was chosen by negotiation.
   std::map<std::string, std::shared_ptr<rclcpp::PublisherBase>> key_to_publisher_;
+  /// The transient local subscription that gathers supported types from NegotiatedSubscriptions.
   rclcpp::Subscription<negotiated_interfaces::msg::SupportedTypes>::SharedPtr supported_types_sub_;
+  /// The timer used for checking for graph changes.
   rclcpp::TimerBase::SharedPtr graph_change_timer_;
+  /// The graph event used for checking for graph changes.
   rclcpp::Event::SharedPtr graph_event_;
+  /// The mutex to protect against concurrent modification of the
+  /// negotiated_subscription_type_gids map.
   std::mutex negotiated_subscription_type_mutex_;
+  /// A map between PublisherGids and the list of unique type keys (as returned by generate_key()).
+  /// This is used to track which GIDs preferences have been met during negotiation.
   std::shared_ptr<std::map<PublisherGid,
     std::vector<std::string>>> negotiated_subscription_type_gids_;
 };
--- negotiated/include/negotiated/negotiated_subscription.hpp
+++ negotiated/include/negotiated/negotiated_subscription.hpp
@@ -34,27 +34,60 @@ namespace negotiated
 namespace detail
 {
 
+/// The callback that will be called when a NegotiatedPublisher sends the results of negotiation.
+/**
+ * This is the function that will determine which choice of the NegotiatedPublisher this particular
+ * NegotiatedSubscription is going to choose.  If this NegotiatedSubscription is already connected
+ * to a particular topic, this callback will prefer to choose that topic again if available.
+ * Otherwise, this callback will choose the first topic in the list that is supported by
+ * this NegotiatedSubscription.
+ *
+ * \param[in] existing_info The information about the existing subscription; may be empty
+ * \param[in] msg The list of topics that the NegotiatedPublisher chose.
+ * \return The topic that this NegotiatedSubscription should connect to.  May be empty, in which
+ *         case no connection will be made.
+ */
 negotiated_interfaces::msg::NegotiatedTopicInfo default_negotiate_cb(
   const negotiated_interfaces::msg::NegotiatedTopicInfo & existing_info,
   const negotiated_interfaces::msg::NegotiatedTopicsInfo & msg);
 
 }  // namespace detail
 
+/// NegotiatedSubscriptionOptions allows the user to control some aspects of NegotiatedSubscription.
 struct NegotiatedSubscriptionOptions
 {
+  /// Whether to automatically disconnect the NegotiatedSubscription on negotiation failure.
+  /// If set to false, will keep subscription active to a topic even if the NegotiatedPublisher
+  /// has failed renegotiation.
   bool disconnect_on_negotiation_failure{true};
 
+  /// A user settable callback to call instead of the default_negotiate_cb().
   std::function<negotiated_interfaces::msg::NegotiatedTopicInfo(
       const negotiated_interfaces::msg::NegotiatedTopicInfo & existing_info,
       const negotiated_interfaces::msg::NegotiatedTopicsInfo & msg)> negotiate_cb{
     detail::default_negotiate_cb};
 };
 
+/// NegotiatedSubscription implements the subscription side of a negotiated system.
+/// It expresses preferences to the NegotiatedPublisher, and then chooses which option to
+/// take after the NegotiatedPublisher negotiates.
 class NegotiatedSubscription
 {
 public:
   RCLCPP_SMART_PTR_DEFINITIONS(NegotiatedSubscription)
 
+  /// Create a new NegotiatedSubscription with the given "base" topic_name.
+  /**
+   * The topic_name given here will be used to initially contact the NegotiatedPublisher.  It will
+   * also be used to get information about the chosen topic_names after negotiation.
+   *
+   * \param[in] node_parameters The node parameters interface to use.
+   * \param[in] node_topics The node topics interface to use.
+   * \param[in] node_logging The node logging interface to use.
+   * \param[in] topic_name The topic name to use for the "base" topic and as the prefix for the
+   *                       final topic name.
+   * \param[in] negotiated_sub_options The options to use.
+   */
   explicit NegotiatedSubscription(
     rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters_,
     rclcpp::node_interfaces::NodeTopicsInterface::SharedPtr node_topics_,
@@ -62,6 +95,16 @@ public:
     const std::string & topic_name,
     const NegotiatedSubscriptionOptions & negotiated_sub_options = NegotiatedSubscriptionOptions());
 
+  /// Create a new NegotiatedSubscription with the given "base" topic_name.
+  /**
+   * The topic_name given here will be used to initially contact the NegotiatedPublisher.  It will
+   * also be used to get information about the chosen topic_names after negotiation.
+   *
+   * \param[in] node The node to use to create the publishers and subscriptions.
+   * \param[in] topic_name The topic name to use for the "base" topic and as the prefix for the
+   *                       final topic name.
+   * \param[in] negotiated_sub_options The options to use.
+   */
   template<typename NodeT>
   explicit NegotiatedSubscription(
     NodeT & node,
@@ -76,6 +119,30 @@ public:
   {
   }
 
+  /// Add a supported callback with a weight.
+  /**
+   * Add a supported callback to this NegotiatedSubscription.  The template argument must have the form:
+   *
+   * struct MyType {
+   *   using MsgT = my_ros::msg::type;
+   *   static const inline std::string supported_type_name = "arbitrary";
+   * };
+   *
+   * As a concrete example:
+   *
+   * struct MyType {
+   *   using MsgT = std_msgs::msg::String;
+   *   static const inline std::string supported_type_name = "arbitrary";
+   * };
+   *
+   * \param[in] weight - The relative weight to assign this supported type; types with higher
+   *                     weights are more likely to be chosen.
+   * \param[in] qos - The Quality of Service parameters to apply if this type is chosen.
+   * \param[in] callback - The user callback to call if data starts flowing on this topic.
+   * \param[in] options - The SubscriptionOptions to apply if this type is chosen.
+   * \throws std::runtime_error if the supported_type_name in the structure is the empty string, OR
+   * \throws std::runtime_error if the exact same type has already been added.
+   */
   template<typename T, typename CallbackT>
   void add_supported_callback(
     double weight,
@@ -115,6 +182,14 @@ public:
     key_to_supported_types_[key_name].sub_factory = factory;
   }
 
+  /// Remove a supported callback.
+  /**
+   * Remove a supported callback from this NegotiatedSubscription.  The template argument must have the
+   * same form as in add_supported_callback(), and must have previously been added by
+   * add_supported_callback().
+   * \throws std::runtime_error if the supported_type_name in the structure is the empty string, OR
+   * \throws std::runtime_error if the type to be removed was not previously added.
+   */
   template<typename T>
   void remove_supported_callback()
   {
@@ -132,36 +207,89 @@ public:
     key_to_supported_types_.erase(key_name);
   }
 
+  /// Start sending preferences to the NegotiatedPublisher.
+  /**
+   * This is separated from the constructor to give the user time to call add_supported_callback()
+   * to express all preferences.  Once that is done, the user should call start.  This may be
+   * more than once to express new preferences to the NegotiatedPublisher, which will generally
+   * cause a renegotiation.
+   */
   void start();
 
+  /// Get the publisher count on the negotiated topic.
+  /**
+   * This is the topic used for negotiation, not for data.
+   *
+   * \return The number of publishers on the negotiated topic.
+   */
   size_t get_negotiated_topic_publisher_count() const;
 
+  /// Get the publisher count on the data topic.
+  /**
+   * This is the topic used for transferring data.  This may be zero in the case that no data topic
+   * has yet been negotiated.
+   *
+   * \return The number of publishers on the data topic.
+   */
   size_t get_data_topic_publisher_count() const;
 
 private:
   struct SupportedTypeInfo final
   {
+    /// The supported type info associated with this type.
     negotiated_interfaces::msg::SupportedType supported_type;
+    /// The factory function associated with this type.
     std::function<rclcpp::SubscriptionBase::SharedPtr(const std::string &)> sub_factory;
   };
 
+  /// Generate the key that is used as an index into the maps.
+  /**
+   * Two of the internal maps are keyed off of the uniqueness of individual types as given by
+   * add_supported_type().  This method is responsible for generating those unique keys.
+   *
+   * \param[in] ros_type_name The canonical ROS type name, like std_msgs/msg/String.
+   * \param[in] supported_type_name The arbitrary (but non-empty) string passed by a user.
+   * \return A string that is the unique key into the maps.
+   */
   std::string generate_key(
     const std::string & ros_type_name,
     const std::string & supported_type_name);
 
+  /// The callback that is called when the NegotiatedPublisher sends the results of a negotation.
+  /**
+   * This method will filter the results to only the ones that this object can possibly support
+   * (based on the types registered with add_supported_callback()), and then call the negotiate_cb
+   * in the options structure.  If the returned type from the negotiated_cb is empty, this will
+   * disconnect from the existing subscription (by default).  If the returned type is not empty,
+   * but is the same as the existing connection, no further action will be taken.
+   * If the returned type is different than the existing connection, then the existing connection
+   * will be broken and a new connection will be made.
+   *
+   * \param[in] msg The NegotiatedTopicsInfo that contains the results of negotiation.
+   */
   void topicsInfoCb(const negotiated_interfaces::msg::NegotiatedTopicsInfo & msg);
 
+  /// The node parameters interface to use.
   rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters_;
+  /// The node topics interface to use.
   rclcpp::node_interfaces::NodeTopicsInterface::SharedPtr node_topics_;
+  /// The node logging interface to use.
   rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logging_;
+  /// The original options to this class provided by the user.
   NegotiatedSubscriptionOptions negotiated_sub_options_;
 
+  /// A map between unique type keys (as returned by generate_key()) and the SupportedTypeInfos.
   std::unordered_map<std::string, SupportedTypeInfo> key_to_supported_types_;
+  /// The subscription used to include this class in the NegotiatedPublisher network and to
+  /// receive preferences from the NegotiatedPublisher once they have been negotiated.
   rclcpp::Subscription<negotiated_interfaces::msg::NegotiatedTopicsInfo>::SharedPtr
     negotiated_subscription_;
+  /// The subscription that data flows over once negotiation has happened.
   std::shared_ptr<rclcpp::SubscriptionBase> subscription_;
+  /// The transient local publisher that informs the NegotiatedPublisher of preferences.
   rclcpp::Publisher<negotiated_interfaces::msg::SupportedTypes>::SharedPtr supported_types_pub_;
 
+  /// Saved information about the currently connected data topic.
   negotiated_interfaces::msg::NegotiatedTopicInfo existing_topic_info_;
 };
 
